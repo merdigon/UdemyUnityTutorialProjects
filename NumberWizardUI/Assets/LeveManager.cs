b14e4c8e84acc33a773d1b8e9ab5517986d7f738
Szymon Nowak <szymon.nowak3848@gmail.com>
2017-05-08 01:25:55 +0200

    Project from section 4

---
+++ NumberWizardUI/Assets/LeveManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeveManager : MonoBehaviour {
+	public void LoadLevel(string name)
+	{
+		if (name.Equals ("quit"))
+			Application.Quit ();
+		else
+			Application.LoadLevel (name);
+	}
+}
---
+++ NumberWizardUI/Assets/NumberWizard.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+	// Use this for initialization
+	int max;
+	int min;
+	int guess;
+	int maxGuessedAllowed = 10;
+	int guessCount;
+
+	public Text guessLabel;
+
+	void Start () {
+		StartGame();
+	}
+	
+	void StartGame () {
+		max = 1000;
+		min = 1;
+		guessCount = 0;
+		NextGuess ();
+	}
+
+	public void UpArrowKey_Pressed()
+	{
+		min = guess;
+		NextGuess();
+	}
+
+	public void DownArrowKey_Pressed()
+	{
+		max = guess;
+		NextGuess();
+	}
+
+	public void EqualKey_Pressed()
+	{
+		Application.LoadLevel ("Lost");
+	}
+	
+	void NextGuess () {
+		guess = Random.Range (min, max+1);
+		guessCount++;
+		if (guessCount >= maxGuessedAllowed)
+			Application.LoadLevel ("Won");
+		else
+			guessLabel.text = guess.ToString();
+	}
+}
